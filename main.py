0379d9157e50dd10173d271564b3fa663546f382
Anderson Madeira <andersonmadeiracs@gmail.com>
2014-06-18 18:32:18 -0300

    Count occurrences
    Count occurrences of every byte of input file.

--- main.py
+++ main.py
@@ -3,8 +3,18 @@
 
 import sys
 import os
+import array
+from collections import Counter
+
+def get_file_content(filename):
+    f = open(filename, 'rb') # input file
+    a = array.array('B') # creates an array of bytes
+    a.fromstring(f.read()) # feeds array with contents of 'filename'
+    print Counter(a) # counts the occurrencies of each byte in the given file
+    
 
 def main(filename):
+    get_file_content(filename)
     pass
 
 def show_usage():
