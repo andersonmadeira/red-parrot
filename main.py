a98c5796db7d8610e552558a63ad0c98ac05d3eb
Anderson Madeira <andersonmadeiracs@gmail.com>
2014-06-19 14:46:35 -0300

    operator module
    Using operator instead of collections module for sorting the dict with
    frequences. I need to add support for arbitrary chunk_size in the future
    for higer compression ratio.

--- main.py
+++ main.py
@@ -4,17 +4,25 @@
 import sys
 import os
 import array
+import operator
 from collections import Counter
 
-def get_file_content(filename):
-    f = open(filename, 'rb') # input file
-    a = array.array('B') # creates an array of bytes
-    a.fromstring(f.read()) # feeds array with contents of 'filename'
-    print Counter(a) # counts the occurrencies of each byte in the given file
-    
+def get_freq_from(filename, chunk_size=1):
+    if chunk_size == 1:
+        f = open(filename, 'rb') # input file
+        a = array.array('B') # creates an array of bytes
+        a.fromstring(f.read()) # feeds array with contents of 'filename'
+        c = Counter(a) # counts the occurrencies of each byte in the given file
+        c_ord = sorted(c.iteritems(), key=operator.itemgetter(1))
+        print c
+        print c_ord
+        print 'Most freq.:', c_ord[len(c_ord)-1] # shows the most frequent byte
+        print 'Least freq.:', c_ord[0] # shows the least frequent byte
+    else: # @TODO: compression using blocks of bytes as words for higer compression ratio com bigger files.
+        pass
 
 def main(filename):
-    get_file_content(filename)
+    get_freq_from(filename)
     pass
 
 def show_usage():
