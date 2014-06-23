6c6f7958bfcad8e0069e9019e292f8c2f049afb6
andersonmadeira <andersonmadeiracs@gmail.com>
2014-06-23 12:17:56 -0300

    HuffmanNode and List of Nodes
    Preparing for the huffman algorithm, now i have a node class that
    implements a __cmp__ method to allow functions to sort lists of nodes.
    get_list_from_dict returns a list of nodes that may be used in the core
    of algorithm to keep lists sorted using bisect module.

--- main.py
+++ main.py
@@ -7,15 +7,44 @@ import array
 import operator
 from collections import Counter
 
+def get_list_from_dict(sorted_dict):
+    """Translates the sorted dict into a sorted list of nodes"""
+    # map 
+    return map(lambda (symbol,freq): HuffmanNode(symbol, freq), sorted_dict)
+
+class HuffmanNode:
+    def __init__(self, symbol=None, freq=None, left=None, right=None):
+        self.symbol = symbol
+        self.freq = freq
+        self.left = left
+        self.right = right
+
+    def __cmp__(self, other): # create a cmp function so nodes may be compared to each other with > < == 
+        if self.freq < other.freq:
+            return -1
+        elif self.fre == other.freq:
+            return 0
+        else:
+            return 1
+
+    def __str__(self):
+        return str(self.symbol) + '->' + str(self.freq)
+
 def get_freq_from(filename, chunk_size=1):
     if chunk_size == 1:
         f = open(filename, 'rb') # input file
         a = array.array('B') # creates an array of bytes
         a.fromstring(f.read()) # feeds array with contents of 'filename'
-        c = Counter(a) # counts the occurrencies of each byte in the given file
+        c = Counter(a) # counts the occurrences of each byte in the given file
         c_ord = sorted(c.iteritems(), key=operator.itemgetter(1))
-        print c
-        print c_ord
+        l = get_list_from_dict(c_ord)
+            
+        print 'Counted dict:',c
+        print 'Sorted dict:', c_ord
+        print 'List:'
+        for e in l:
+            print str(e),
+        print
         print 'Most freq.:', c_ord[len(c_ord)-1] # shows the most frequent byte
         print 'Least freq.:', c_ord[0] # shows the least frequent byte
     else: # @TODO: compression using blocks of bytes as words for higer compression ratio com bigger files.
